5bb15bafa2395b4975870d58a8489ff7737ba12f
sSimo <xsimone97@hotmail.it>
2018-08-31 02:43:30 +0200

    Added script for mono input mode and working VrMode-Normal view mode switch

--- Assets/Scripts/ToogleVR.cs
+++ Assets/Scripts/ToogleVR.cs
@@ -5,11 +5,19 @@ using UnityEngine.XR;
 
 public class ToogleVR : MonoBehaviour {
 
+    public GameObject MyCam;
     bool vrMode = true;
+
+    private void Start()
+    {
+        MyCam.GetComponent<GyroInput>().enabled = !vrMode;
+    }
 
     public void ToggleVRMode()
     {
         vrMode = !vrMode;
         XRSettings.enabled = vrMode;
+        
+        MyCam.GetComponent<GyroInput>().enabled = !vrMode;
     }
 }
